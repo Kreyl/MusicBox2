96af3b3917140b8173a6e3bc198ca14484e6e6f7
Eldalim\Elessar <elessarcuthalion@mail.ru>
2017-10-29 12:33:40 +0200

    Возврат старой версии драйвера

--- FirmwareNew/ws2812b.cpp
+++ FirmwareNew/ws2812b.cpp
@@ -14,21 +14,21 @@
                         | STM32_DMA_CR_DIR_M2P  /* Direction is memory to peripheral */
 
 // Tx timings: bit cnt
-#define SEQ_1               0b1110  // 0xE
-#define SEQ_0               0b1000  // 0x8
+#define SEQ_1               0b11111000  // 0xF8
+#define SEQ_0               0b11000000  // 0xC0
 
-#define SEQ_00              0x88
-#define SEQ_01              0x8E
-#define SEQ_10              0xE8
-#define SEQ_11              0xEE
+#define SEQ_00              0x8080
+#define SEQ_01              0x80E0
+#define SEQ_10              0xE080
+#define SEQ_11              0xE0E0
 
 void IntelLeds_t::Init() {
     PinSetupAlterFunc(LEDWS_PIN);
-    ISpi.Setup(boMSB, cpolIdleLow, cphaFirstEdge, sclkDiv4, bitn16);
+    ISpi.Setup(boMSB, cpolIdleLow, cphaFirstEdge, sclkDiv2, bitn16);
     ISpi.Enable();
     ISpi.EnableTxDma();
 
-    Uart.Printf("Led BufSz=%u bytes\r", sizeof(IBuf));
+//    Uart.Printf("Led BufSz=%u bytes\r", sizeof(IBuf));
 
     // Zero buffer
     for(int i=0; i<TOTAL_W_CNT; i++) IBuf[i] = 0;
@@ -40,44 +40,44 @@ void IntelLeds_t::Init() {
 }
 
 void IntelLeds_t::AppendBitsMadeOfByte(uint8_t Byte) {
-    uint8_t Bits, bMsb = 0, bLsb = 0;
+    uint8_t Bits;
     Bits = Byte & 0b11000000;
-    if     (Bits == 0b00000000) bMsb = SEQ_00;
-    else if(Bits == 0b01000000) bMsb = SEQ_01;
-    else if(Bits == 0b10000000) bMsb = SEQ_10;
-    else if(Bits == 0b11000000) bMsb = SEQ_11;
+    if     (Bits == 0b00000000) *PBuf++ = SEQ_00;
+    else if(Bits == 0b01000000) *PBuf++ = SEQ_01;
+    else if(Bits == 0b10000000) *PBuf++ = SEQ_10;
+    else if(Bits == 0b11000000) *PBuf++ = SEQ_11;
 
     Bits = Byte & 0b00110000;
-    if     (Bits == 0b00000000) bLsb = SEQ_00;
-    else if(Bits == 0b00010000) bLsb = SEQ_01;
-    else if(Bits == 0b00100000) bLsb = SEQ_10;
-    else if(Bits == 0b00110000) bLsb = SEQ_11;
-
-    *PBuf++ = (bMsb << 8) | bLsb;
+    if     (Bits == 0b00000000) *PBuf++ = SEQ_00;
+    else if(Bits == 0b00010000) *PBuf++ = SEQ_01;
+    else if(Bits == 0b00100000) *PBuf++ = SEQ_10;
+    else if(Bits == 0b00110000) *PBuf++ = SEQ_11;
 
     Bits = Byte & 0b00001100;
-    if     (Bits == 0b00000000) bMsb = SEQ_00;
-    else if(Bits == 0b00000100) bMsb = SEQ_01;
-    else if(Bits == 0b00001000) bMsb = SEQ_10;
-    else if(Bits == 0b00001100) bMsb = SEQ_11;
+    if     (Bits == 0b00000000) *PBuf++ = SEQ_00;
+    else if(Bits == 0b00000100) *PBuf++ = SEQ_01;
+    else if(Bits == 0b00001000) *PBuf++ = SEQ_10;
+    else if(Bits == 0b00001100) *PBuf++ = SEQ_11;
 
     Bits = Byte & 0b00000011;
-    if     (Bits == 0b00000000) bLsb = SEQ_00;
-    else if(Bits == 0b00000001) bLsb = SEQ_01;
-    else if(Bits == 0b00000010) bLsb = SEQ_10;
-    else if(Bits == 0b00000011) bLsb = SEQ_11;
+    if     (Bits == 0b00000000) *PBuf++ = SEQ_00;
+    else if(Bits == 0b00000001) *PBuf++ = SEQ_01;
+    else if(Bits == 0b00000010) *PBuf++ = SEQ_10;
+    else if(Bits == 0b00000011) *PBuf++ = SEQ_11;
 
-    *PBuf++ = (bMsb << 8) | bLsb;
 }
 
 void IntelLeds_t::ISetCurrentColors() {
-    PBuf = IBuf + (RST_W_CNT / 2);    // First words are zero to form reset
     // Fill bit buffer
+    PBuf = IBuf;
+    PBuf++;
     for(uint32_t i=0; i<LED_CNT; i++) {
         AppendBitsMadeOfByte(ICurrentClr[i].G);
         AppendBitsMadeOfByte(ICurrentClr[i].R);
         AppendBitsMadeOfByte(ICurrentClr[i].B);
     }
+//    for(uint32_t i=0; i<TOTAL_W_CNT; i++)
+//        Uart.PrintfNow("\r send n %u data %X ", i, IBuf[i]);
     // Start transmission
     dmaStreamDisable(LEDWS_DMA);
     dmaStreamSetMemory0(LEDWS_DMA, IBuf);
@@ -85,3 +85,4 @@ void IntelLeds_t::ISetCurrentColors() {
     dmaStreamSetMode(LEDWS_DMA, LED_DMA_MODE);
     dmaStreamEnable(LEDWS_DMA);
 }
+
--- FirmwareNew/ws2812b.h
+++ FirmwareNew/ws2812b.h
@@ -11,27 +11,30 @@
  * ========== WS2812 control module ==========
  * Only basic command "SetCurrentColors" is implemented, all other is up to
  * higher level software.
- * SPI input frequency should be 8 MHz (which results in 4MHz bitrate)
+ * SPI input frequency should be 16 MHz (which results in 8MHz bitrate)
  */
 
 
 #include "ch.h"
 #include "hal.h"
 #include "kl_lib.h"
-#include "color.h"
 #include "uart.h"
 
 #ifndef LED_CNT
-#define LED_CNT             24   // Number of WS2812 LEDs
+#define LED_CNT             5   // Number of WS2812 LEDs
 #endif
-// Do not touch
-#define SEQ_LEN             4
-#define RST_W_CNT           4 // zero words before and after data to produce reset
+
+#define SEQ_LEN             8
+#define RST_W_CNT           4   // zero words after data to produce reset
 
 // SPI16 Buffer (no tuning required)
 #define DATA_BIT_CNT        (LED_CNT * 3 * 8 * SEQ_LEN)   // Each led has 3 channels 8 bit each
 #define DATA_W_CNT          ((DATA_BIT_CNT + 15) / 16)
 #define TOTAL_W_CNT         (DATA_W_CNT + RST_W_CNT)
+
+typedef struct {
+    uint8_t R, G, B;
+} ColorWS_t;
 
 class IntelLeds_t {
 private:
@@ -41,14 +44,7 @@ private:
     void AppendBitsMadeOfByte(uint8_t Byte);
 public:
     void Init();
-    bool AreOff() {
-        for(uint8_t i=0; i<LED_CNT; i++) {
-            if(ICurrentClr[i] != clBlack) return false;
-        }
-        return true;
-    }
     // Inner use
-    Color_t ICurrentClr[LED_CNT];
+    ColorWS_t ICurrentClr[LED_CNT];
     void ISetCurrentColors();
-    void ITmrHandlerI();
 };
